f7d89a2e84e2291d8c7aa195192a251646a7fea4
musicalmacdonald <musicalmacdonald@gmail.com>
2018-03-11 01:15:53 -0700

    Fixed race up and race down

--- led_functions.py
+++ led_functions.py
@@ -12,41 +12,48 @@ def blink_led_once(blink_frequency, port):
     sleep(blink_frequency / 2)
 
 
-def blink_led_forever():
+def blink_led_forever(port):
     """blink the led once per second in a never ending loop"""
     while True:
-        blink_led_once(1)
+        blink_led_once(1, port)
 
 
-def race_led(blink_frequency):
-    # led 1
-    blink_led_once(blink_frequency, 18)
-    # led 2
-    blink_led_once(blink_frequency, 23)
-    # led 3
-    blink_led_once(blink_frequency, 25)
-    # led 4
-    blink_led_once(blink_frequency, 12)
-    # led 5
-    blink_led_once(blink_frequency, 16)
-    # led 6
-    blink_led_once(blink_frequency, 20)
-    # led 7
-    blink_led_once(blink_frequency, 21)
-    # led 8
-    blink_led_once(blink_frequency, 26)
-    # led 9
-    blink_led_once(blink_frequency, 19)
-    # led 10
-    blink_led_once(blink_frequency, 13)
+def race_led_up(blink_frequency):
+
+    led_ports = [18, 23, 25, 12, 16, 20, 21, 26, 19, 13]
+    
+    p=0
+    for i in led_ports:
+        blink_led_once(blink_frequency, led_ports[p])
+        #print("Activating port:", led_ports[p])
+        p+=1
+    
+
+def race_led_down(blink_frequency):
+
+    led_ports = [18, 23, 25, 12, 16, 20, 21, 26, 19, 13]
+
+    p=1
+    for i in led_ports:
+        blink_led_once(blink_frequency, led_ports[-p])
+        #print("Activating port ", led_ports[-p])
+        p+=1
 
 
-if __name__ == "__main__":
+def test_leds():
     print("This will make the LED blink once")
     blink_led_once(2, 18)
 
-    print ("This will make them race")
-    race_led(1)
+    print("This will make them race")
+    race_led_up(.5)
+    race_led_down(.5)
 
     print("Come on, we can go faster!")
-    race_led(.5)
+    race_led_up(.2)
+    race_led_down(.2)
+    race_led_up(.1)
+    race_led_down(.1)
+
+
+if __name__ == "__main__":
+    test_leds()
