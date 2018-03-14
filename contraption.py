5b3979812b9e166317e05b71a511d4a3187897a2
Megan <musicalmacdonald@gmail.com>
2018-03-14 15:25:11 -0600

    added race_up function for testing

--- contraption.py
+++ contraption.py
@@ -8,12 +8,15 @@ class PiLedContraption:
 
     _led_ports = []
     _leds = []
+    _led_index_array = []
 
     def __init__(self):
 
         self._led_ports = [18, 23, 25, 12, 16, 20, 21, 26, 19, 13]
         for i in self._led_ports:
             self._leds.append(LED(i))
+        self._led_index_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self._st = .5
 
     def led_on(self, led_index):
         if not 0 <= led_index < 10:
@@ -31,29 +34,30 @@ class PiLedContraption:
             self._leds[led_index].off()
             #self._led_ports[led_index] = 0
 
-    '''
     def race_up(self):
-        for i in led_ports:
-            blink_led_once(blink_frequency, i)
+        for i in _led_index_array:
+            led_on(i)
+            sleep(self._st)
             # print("Activating port ", i)
 
-    def race_down(self):
+    '''def race_down(self):
         
-        for i in led_ports:
+        for i in _leds:
             blink_led_once(blink_frequency, reversed(i))
-            # print("Activating port ", reversed(i))
-            
-        '''
+            # print("Activating port ", reversed(i))'''
+
 
     def dance_randomly(self):
         pass
 
 
 if __name__ == "__main__":
+    #test class initiation
     test = PiLedContraption()
     print(type(test))
 
-    print("\nTesting LED 0")
+    #test on/off
+    test1= {print("\nTesting LED 0")
     test.led_on(0)
     sleep(.5)
 
@@ -65,19 +69,12 @@ if __name__ == "__main__":
     test.led_on(10)
     sleep(.5)
 
-    print("\nTesting invalid LED off")
-    test.led_off(10)
-    sleep(.5)
+    #race up/ race down
+    print ("Testing race up")
+    test.race_up()
+    print ("Testing race down")
+    test.race_down()
 
-    print("\nTesting LED 1")
-    test.led_on(1)
-    sleep(.5)
 
-    print("\nTesting LED 2 off")
-    test.led_off(2)
-    sleep(.5)
-    
-    print("\nTesting LED 1 off")
-    test.led_off(1)
 
 
