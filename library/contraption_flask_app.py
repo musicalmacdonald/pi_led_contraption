ef1f2ccce97b042b57eb49a1a1bdd40090f2cad4
Megan <musicalmacdonald@gmail.com>
2018-04-14 14:50:21 -0600

    post working for all pages, fixed print in led_off

--- library/contraption_flask_app.py
+++ library/contraption_flask_app.py
@@ -2,7 +2,8 @@
 """
 
 from flask import Flask, render_template, request
-from library.mocpi import contraption
+#from library.mocpi import contraption
+from library.raspi import contraption
 
 
 app = Flask(__name__)
@@ -22,22 +23,115 @@ def index_2():
 @app.route('/individual.html', methods=['GET', 'POST'])
 def individual():
     if request.method == 'POST':
-        print("You posted")
+        print("You posted to individual.html")
         print(request.form)
         led_number = request.form["led_number"]
         print(led_number)
         ledcon.led_on(int(led_number))
     elif request.method == 'GET':
-        print("Here's your web-page")
+        print("Here's 'Individual' web-page")
     return render_template("individual.html")
 
 
-@app.route('/group.html')
+@app.route('/group.html', methods=['GET', 'POST'])
 def group():
+    if request.method == 'POST':
+        print("You posted to group.html")
+        print(request.form)
+
+        #LED 0
+        if 'on' == request.form['led_number0']:
+            print(request.form['led_number0'])
+            print("flask: led 0 is on")
+            ledcon.led_on(0)
+        else:
+            print("flask:Led 0 is off")
+            ledcon.led_off(0)
+
+
+        #LED 1
+        if request.form['led_number1'] == 'on':
+            print("flask: led 1 is on")
+            ledcon.led_on(1)
+
+        else:
+            print("flask:Led 1 is off")
+            ledcon.led_off(1)
+
+        #LED 2
+        if request.form['led_number2'] == 'on':
+            print("flask: led 2 is on")
+            ledcon.led_on(2)
+        else:
+            print("flask:Led 2 is off")
+            ledcon.led_off(2)
+
+        #LED 3
+        if request.form['led_number3'] == 'on':
+            ledcon.led_on(3)
+        else:
+            ledcon.led_off(3)
+
+        #LED 4
+        if request.form['led_number4'] == 'on':
+            ledcon.led_on(4)
+        else:
+            ledcon.led_off(4)
+
+        #LED 5
+        if request.form['led_number5'] == 'on':
+            ledcon.led_on(5)
+        else:
+            ledcon.led_off(5)
+
+        #LED 6
+        if request.form['led_number6'] == 'on':
+            ledcon.led_on(6)
+        else:
+            ledcon.led_off(6)
+
+        #LED 7
+        if request.form['led_number7'] == 'on':
+            ledcon.led_on(7)
+        else:
+            ledcon.led_off(7)
+
+        #LED 8
+        if request.form['led_number8'] == 'on':
+            ledcon.led_on(8)
+        else:
+            ledcon.led_off(8)
+
+        #LED 9
+        if request.form['led_number9'] == 'on':
+            ledcon.led_on(9)
+        else:
+            ledcon.led_off(9)
+
+    elif request.method == 'GET':
+        print("Here's 'Group' web-page")
+
     return render_template("group.html")
 
 
-@app.route('/patterns.html')
+@app.route('/patterns.html', methods=['GET', 'POST'])
 def patterns():
+    if request.method == 'POST':
+        print("You posted to patterns.html")
+        print(request.form)
+        p=request.form['pattern']
+        '''A nice way to improve debugability'''
+        print(p)
+        if request.form['pattern'] == 'Race Up':
+            print("flask race up")
+            ledcon.race_up()
+        elif request.form['pattern'] == 'Race Down':
+            print("flask: race down")
+            ledcon.race_down()
+        elif request.form['pattern'] == 'Dance!':
+            print("flask: dancing")
+            ledcon.dance()
+    elif request.method == 'GET':
+        print("Here's 'Patterns' web-page")
     return render_template("patterns.html")
 
--- library/raspi/contraption.py
+++ library/raspi/contraption.py
@@ -1,11 +1,11 @@
 """This class will create LED functions for use on a web-based application."""
 
-#from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 import random
 
-class PiLedContraption:
 
+class PiLedContraption:
     _led_ports = []
     _leds = []
     _led_index_array = []
@@ -14,8 +14,8 @@ class PiLedContraption:
 
         self._led_ports = [18, 23, 25, 12, 16, 20, 21, 26, 19, 13]
         for i in self._led_ports:
-            #self._leds.append(LED(i))
-            self._leds.append(i)
+            self._leds.append(LED(i))
+            #self._leds.append(i)
         self._led_index_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         self._st = .2
 
@@ -24,54 +24,52 @@ class PiLedContraption:
             print("{} is not a valid LED index".format(led_index))
         else:
             print("LED {} is on.".format(led_index))
-            #self._leds[led_index].on()
-            self._led_ports[led_index] = 1
+            self._leds[led_index].on()
+            #self._led_ports[led_index] = 1
 
     def led_off(self, led_index):
         if not 0 <= led_index < 10:
             print("{} is not a valid LED index".format(led_index))
         else:
             print("LED {} is off.".format(led_index))
-            #self._leds[led_index].off()
-            self._led_ports[led_index] = 0
+            self._leds[led_index].off()
+            #self._led_ports[led_index] = 0
 
     def race_up(self):
         print("Racing up")
 
         for i in self._led_index_array:
-            #self.led_on(i)
+            self.led_on(i)
             sleep(self._st)
-            #self.led_off(i)
+            self.led_off(i)
             print("Activating port ", i)
 
     def race_down(self):
 
         print("Racing Down")
         for i in reversed(self._led_index_array):
-            # self.led_on(i)
+            self.led_on(i)
             sleep(self._st)
-            # self.led_off(i)
+            self.led_off(i)
             print("Activating port ", i)
-
 
     def dance(self):
         print("Dancing")
         random.seed()
-        for i in range(0,20):
-            r=random.randint(0,9)
+        for i in range(0, 20):
+            r = random.randint(0, 9)
             self.led_on(r)
             sleep(self._st)
             self.led_off(r)
             sleep(self._st)
-            
 
 
 if __name__ == "__main__":
-    #test class initiation
+    # test class initiation
     test = PiLedContraption()
     print(type(test))
 
-    #test on/off
+    # test on/off
     print("\nTesting LED 0")
     test.led_on(0)
     sleep(.5)
@@ -84,15 +82,15 @@ if __name__ == "__main__":
     test.led_on(10)
     sleep(.5)
 
-    #race up/ race down
-    print ("\nTesting race up")
+    # race up/ race down
+    print("\nTesting race up")
     test.race_up()
     sleep(1)
-    print ("\nTesting race down")
+    print("\nTesting race down")
     test.race_down()
-    
-    #dance test
-    print ("\nTesting dance dance revolution!")
+
+    # dance test
+    print("\nTesting dance dance revolution!")
     test.dance()
 
 
